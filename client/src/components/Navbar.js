2e98b53a3873452accc9cd5c03b75645e413dc31
Kris <kris.fillmore@gmail.com>
2021-09-14 15:07:16 -0700

    responsive prototype

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,18 +1,34 @@
-import React from 'react'
-import {Navbar, Nav} from 'react-bootstrap'
+import { Navbar, Nav } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
 
-function navbar() {
-    return (
-        <>
+function OurNavbar() {
+  const [width, setWindowWidth] = useState([0, 0]);
+  let title = "Eltek Charger";
+
+  if (width[0] < 800) {
+    title = "E";
+  }
+  console.log(width);
+  useEffect(() => {
+    function updateSize() {
+      setWindowWidth([window.innerWidth, window.innerHeight]);
+    }
+    window.addEventListener("resize", updateSize);
+    updateSize();
+    return () => window.removeEventListener("resize", updateSize);
+  }, []);
+
+  return (
+    <>
       <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="Home">Eltek Charger</Navbar.Brand>
+        <Navbar.Brand href="Home">{title}</Navbar.Brand>
         <Nav className="mr-auto">
           <Nav.Link href="Dashboard">Dashboard</Nav.Link>
           <Nav.Link href="Config">Config</Nav.Link>
         </Nav>
       </Navbar>
     </>
-    )
+  );
 }
 
-export default navbar
+export default OurNavbar;
